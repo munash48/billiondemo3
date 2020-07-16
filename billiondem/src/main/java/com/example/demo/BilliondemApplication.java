e8d89d49158095445eb495795aeac3a06396e8c9
munash48 <mugwanyan@gmail.com>
2020-07-16 19:29:42 +0300

    bulk commit

---
+++ billiondem/src/main/java/com/example/demo/BilliondemApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilliondemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BilliondemApplication.class, args);
+	}
+
+}
